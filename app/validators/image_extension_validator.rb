393620b82eed5d735fb42f95151bdd44472dd3d2
matsuhisa <matsuhisa+github@gmail.com>
2016-05-02 11:51:34 +0900

    Tweak validate for has_many

--- app/validators/image_extension_validator.rb
+++ app/validators/image_extension_validator.rb
@@ -1,7 +1,7 @@
 class ImageExtensionValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless ['.jpg', '.jpeg', '.png', '.gif'].include?(value.downcase)
-      record.errors[attribute] << "はJPEG、PNG、GIF形式のファイルのみです"
+      record.errors[attribute] << (options[:message] || "はJPEG、PNG、GIF形式のファイルのみです")
     end
   end
 end
